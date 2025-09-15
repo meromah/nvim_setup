eea9a1ae449fe58cae219e954332357f39276f7f
gotta_hide_my_nick <124768973+meromah@users.noreply.github.com>
2025-09-15 06:04:55 +0200

    initial commit

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,14 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.showcmd = true
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+vim.opt.number = true
+
+vim.opt.guifont = 'BitstreamVeraSandMono_NF:h14'
