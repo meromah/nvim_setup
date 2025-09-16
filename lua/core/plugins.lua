9c4431552753262513d303ca67446cad0b48a0be
gotta_hide_my_nick <124768973+meromah@users.noreply.github.com>
2025-09-16 04:21:48 +0200

    new commit

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -11,6 +11,8 @@ end
 
 local packer_bootstrap = ensure_packer()
 
+vim.cmd [[filetype plugin indent on]]
+
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'ellisonleao/gruvbox.nvim'
