ed127e962b517833390baca61748e9c9d449354d
gotta_hide_my_nick <124768973+meromah@users.noreply.github.com>
2025-09-16 18:47:20 +0200

    terminal toggle as a bottom window as a function has been added.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -18,3 +18,35 @@ vim.opt.relativenumber = true
 
 
 vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true, silent = true })
+
+
+vim.api.nvim_create_autocmd('TermOpen', {
+    group = vim.api.nvim_create_augroup('custom-term-open', { clear = true}),
+    callback = function()
+        vim.opt.number = false
+        vim.opt.relativenumber = false
+    end,
+})
+
+
+-- normal mode - toggle (new, close) terminal as a bottom window
+vim.keymap.set('n', '<Space>t', function()
+
+    local window = vim.api.nvim_get_current_win()
+
+    if vim.bo.buftype == 'terminal' then
+        vim.api.nvim_win_close(window, true)
+        return
+    end
+
+    -- otherwise
+    vim.cmd('vnew')
+    vim.cmd('term')
+    vim.cmd('wincmd J')
+
+    vim.api.nvim_win_set_height(window, 12)
+    vim.cmd('startinsert')
+end, { noremap = true, silent = true })
+
+
+vim.keymap.set('t', 'jk', [[<C-\><C-n>]], { noremap = true, silent = true })
