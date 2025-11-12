12dbae074d30a2babbb9d1199060f4dd9a3782ce
meromah <124768973+meromah@users.noreply.github.com>
2025-11-12 23:04:02 +0100

    new plugin that allows nvim to have project specific configurations

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -51,6 +51,18 @@ require("lazy").setup({
         },
       },
     },
+  },
+  {
+    "klen/nvim-config-local",
+    opts = {
+      config_files = {".nvim.lua"},
+      hashfile = vim.fn.stdpath("data") .. "/config-local",
+
+      autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
+      commands_create = true,     -- Create commands (ConfigLocalSource, ConfigLocalEdit, ConfigLocalTrust, ConfigLocalDeny)
+      silent = false,             -- Disable plugin messages (Config loaded/denied)
+      lookup_parents = false,     -- Lookup config files in parent directories
+    }
   }
 })
 
