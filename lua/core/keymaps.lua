b4d34d1174f0feb18945ba1087840a82bde0e216
Meromah <124768973+meromah@users.noreply.github.com>
2026-04-26 00:41:26 +0200

    cursorline is accessive visual stimulant; removed;

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,7 +3,7 @@ vim.g.maplocalleader = ' '
 
 vim.opt.showcmd = true
 vim.opt.autowrite = true
-vim.opt.cursorline = true
+-- vim.opt.cursorline = true
 vim.opt.autoread = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
