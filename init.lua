497cb4f65dd5f1c7d59176d3c0b311120cabb072
Meromah <124768973+meromah@users.noreply.github.com>
2026-05-24 02:51:39 +0200

    colorscheme has been set to habamax

--- init.lua
+++ init.lua
@@ -5,3 +5,11 @@ require("core.plugin_config")
 local options = {
     termguicolors = true
 }
+
+vim.cmd("colorscheme habamax")
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "CursorLine", { bg = "#2e3a4a" })
+  end,
+})
