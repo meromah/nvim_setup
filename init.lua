e6db01ebced583b020cb7e8a062be984baceb5c8
Meromah <124768973+meromah@users.noreply.github.com>
2026-06-03 04:48:09 +0200

    diagnostics specific configurtions/keymaps/preference has been added;
    first one makes diagnostics text appear as floating window;
    second oen is keymap to show it with ctrl+k
    third one makes that same floating window appear upon cursor focus

--- init.lua
+++ init.lua
@@ -13,3 +13,28 @@ vim.api.nvim_create_autocmd("ColorScheme", {
     vim.api.nvim_set_hl(0, "CursorLine", { bg = "#2e3a4a" })
   end,
 })
+
+-- vim.diagnostic.config({
+--   virtual_text = true,
+-- })
+
+-- vim.keymap.set("n", "<C-k>", function()
+--   vim.diagnostic.open_float(nil, {
+--     focusable = false,
+--     -- border = "rounded",
+--     scope = "cursor",
+--   })
+-- end, { desc = "Show diagnostics under cursor" })
+
+-- pop diagnostic info but running :lua print(vim.inspect(vim.diagnostic.config()))
+
+vim.o.updatetime = 300
+
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    vim.diagnostic.open_float(nil, {
+      focusable = false,
+      scope = "cursor",
+    })
+  end,
+})
