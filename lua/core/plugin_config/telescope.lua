c9ed94c6da6a58a29df563daedf6835b44482578
Meroj <sohibilmeroj@diverta.co.jp>
2026-08-12 18:16:02 +0900

    line numbers in the telescope previewer; easier to locate a grep hit
    Telescope creates preview windows with number/relativenumber off. Turned
    both on via the TelescopePreviewerLoaded event, skipping help buffers
    where the numbers are noise.

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -5,3 +5,13 @@ vim.keymap.set('n', '<Space><Space>', builtin.oldfiles, {})
 vim.keymap.set('n', '<Space>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<Space>fh', builtin.help_tags, {})
 
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "TelescopePreviewerLoaded",
+  callback = function(args)
+    if args.data.filetype ~= "help" then
+      vim.wo.number = true
+      vim.wo.relativenumber = true
+    end
+  end,
+})
