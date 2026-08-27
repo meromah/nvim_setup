bfb7d03e0be16621f7127b3c571ac2791aa21021
Meroj <sohibilmeroj@diverta.co.jp>
2026-08-27 15:16:25 +0900

    keymaps: remove the <Space>t terminal toggle
    It was an exact-match mapping, so any longer <Space>t? binding made
    Neovim wait out 'timeoutlen' on every press and open a terminal if you
    hesitated. :term still works, as do the TermOpen autocmd that hides line
    numbers and the jk/JK mappings for leaving terminal mode.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -35,27 +35,6 @@ vim.api.nvim_create_autocmd('TermOpen', {
     end,
 })
 
-
--- normal mode - toggle (new, close) terminal as a bottom window
-vim.keymap.set('n', '<Space>t', function()
-
-    local window = vim.api.nvim_get_current_win()
-
-    if vim.bo.buftype == 'terminal' then
-        vim.api.nvim_win_close(window, true)
-        return
-    end
-
-    -- otherwise
-    vim.cmd('vnew')
-    vim.cmd('term')
-    vim.cmd('wincmd J')
-
-    vim.api.nvim_win_set_height(window, 25)
-    vim.cmd('startinsert')
-end, { noremap = true, silent = true })
-
-
 vim.keymap.set('t', 'jk', [[<C-\><C-n>]], { noremap = true, silent = true })
 vim.keymap.set('t', 'JK', [[<C-\><C-n>]], { noremap = true, silent = true })
 
